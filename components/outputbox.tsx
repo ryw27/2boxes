f2d5177dd7532f2edb0ed97004f722e9914796e6
ryw27 <rywbook06@gmail.com>
2025-02-25 22:20:10 -0500

    outputbox

---
+++ components/outputbox.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React, { useState, useRef, useEffect }  from 'react';
+
+export default function OutputBox() {
+    return (
+        <div className="flex">
+
+        </div>
+    );
+}
