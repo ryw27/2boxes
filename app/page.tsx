beedc10f8f9f2d047cdb00b9fd0f81e02652a204
ryw27 <rywbook06@gmail.com>
2025-03-05 22:36:07 -0500

    ui changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
-import { VoiceRecorder } from '@/components/voicerecorder';
+import  VoiceRecorder from '@/components/voicerecorder';
 
 import OutputBox from '@/components/outputbox';
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen space-x-2">
-      <div className="container mx-auto p-4 mb-2">
-        <VoiceRecorder/>
+    <div className="flex flex-col space-y-2 p-8">
+      <div className="flex flex-col max-w-4xl rounded-lg border-2 border-gray-200 
+                      container mx-auto p-8 mb-2 bg-white space-y-2">
+        <h1 className="font-bold text-2xl">
+          2 Boxes!!!!!
+        </h1>        
+        <p className="text-gray-700">
+          Record 
+        </p>
+        <div className="flex justify-center items-center"> 
+          <VoiceRecorder/>
+        </div>
       </div>
-      <div className="flex flex-1">
+      <div className="flex bg-gray-500">
         <OutputBox/>
       </div>
     </div>
